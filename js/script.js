6c5bc79da06d3884bba2cff48d9bc1afbcb4f05c
CyrilDenoyelle <Cyril-denoyelle@hotmail.fr>
2017-02-27 21:54:52 +0100

    ajout de la totalité du presque

--- js/script.js
+++ js/script.js
@@ -2,6 +2,25 @@
 	app = {
 		init: function(){
 			console.log('appinit');
+			this.listener()
+		},
+		listener: function(){
+			var r1 = 255, g1 = 255, b1 = 255, r2 = 0, g2 = 0, b2 = 0, degr = 45;
+
+			$('body').css("background", "linear-gradient("+degr+"deg, rgb("+r1+","+g1+","+b1+"), rgb("+r2+","+g2+","+b2+"))");
+			$('input').change(function(){
+				name = $(this).attr('name');
+				val = parseInt($(this).val());
+				if(name == "r1"){r1 = val;}
+				if(name == "g1"){g1 = val;}
+				if(name == "b1"){b1 = val;}
+				if(name == "r2"){r2 = val;}
+				if(name == "g2"){g2 = val;}
+				if(name == "b2"){b2 = val;}
+				if(name == "degr"){degr = val;}
+				// console.log(r1, g1, b1, r2, g2, b2);
+				$('body').css("background", "linear-gradient("+degr+"deg, rgb("+r1+","+g1+","+b1+"), rgb("+r2+","+g2+","+b2+"))");
+			})
 		}
 	}
 })()
